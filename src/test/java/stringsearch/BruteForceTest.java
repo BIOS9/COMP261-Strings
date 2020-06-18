7fc09b6050bee1da2ef777ccf642588824226028
NightFish <nightfish.net@gmail.com>
2020-06-19 02:00:25 +1200

    Add correctness testing for string search

--- src/test/java/stringsearch/BruteForceTest.java
+++ src/test/java/stringsearch/BruteForceTest.java
@@ -1,4 +1,36 @@
 package test.java.stringsearch;
 
+import main.java.stringsearch.BruteForce;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 public class BruteForceTest {
+    @Test
+    public void testWarAndPeace() throws IOException {
+        StringSearchTest.testStringSearch(new BruteForce(), Files.readAllLines(Paths.get("data/war_and_peace.txt")).stream().collect(Collectors.joining("\n")));
+    }
+
+    @Test
+    public void testTaisho() throws IOException {
+        StringSearchTest.testStringSearch(new BruteForce(), Files.readAllLines(Paths.get("data/taisho.txt")).stream().collect(Collectors.joining("\n")));
+    }
+
+    @Test
+    public void testPi() throws IOException {
+        StringSearchTest.testStringSearch(new BruteForce(), Files.readAllLines(Paths.get("data/pi.txt")).stream().collect(Collectors.joining("\n")));
+    }
+
+    @Test
+    public void testLenna() throws IOException {
+        StringSearchTest.testStringSearch(new BruteForce(), Files.readAllLines(Paths.get("data/Lenna.txt")).stream().collect(Collectors.joining("\n")));
+    }
+
+    @Test
+    public void testApollo() throws IOException {
+        StringSearchTest.testStringSearch(new BruteForce(), Files.readAllLines(Paths.get("data/Apollo.txt")).stream().collect(Collectors.joining("\n")));
+    }
 }
---
+++ src/test/java/stringsearch/StringSearchTest.java
@@ -0,0 +1,33 @@
+package test.java.stringsearch;
+
+import main.java.stringsearch.StringSearcher;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+
+public class StringSearchTest {
+    public static void testStringSearch(StringSearcher searcher, String data) {
+        Set<String> wordlist = new HashSet<>();
+        wordlist.addAll(data.chars().parallel().distinct().boxed().map((x) -> String.valueOf((char)x.intValue())).collect(Collectors.toSet()));
+        wordlist.addAll(Arrays.stream(data.split("[ \\s-]")).parallel().collect(Collectors.toSet()));
+        wordlist.remove("");
+        assumeFalse(wordlist.isEmpty());
+
+        System.out.println("Testing on " + wordlist.size() + " patterns.");
+
+        for(String word : wordlist) {
+            searcher.setPattern(word);
+            assertEquals(word, searcher.getPattern());
+
+            int searcherIndex = searcher.search(data);
+            int oracleIndex = data.indexOf(word);
+
+            if(searcherIndex != oracleIndex) {
+                fail("Searcher index (" + searcherIndex + ") and oracle index (" + oracleIndex + ") do not match for pattern \"" + word + "\" pattern length: " + word.length());
+            }
+        }
+    }
+}
