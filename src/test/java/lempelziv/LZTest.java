6c32b6c83e4f5bbd93300a03b70a5366cd87ae16
NightFish <nightfish.net@gmail.com>
2020-06-20 07:10:48 +1200

    Add more testing for LZ, uneven pattern and data sizes

--- src/test/java/lempelziv/LZTest.java
+++ src/test/java/lempelziv/LZTest.java
@@ -4,6 +4,7 @@ import main.java.lempelziv.LempelZiv;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class LZTest {
     @Test
@@ -36,5 +37,42 @@ public class LZTest {
         int[] result = LempelZiv.findPrefix(test, 5, test.length, 0, 5);
         assertEquals(3, result[0], "Best match index was wrong.");
         assertEquals(1, result[1], "Best match length was wrong.");
+    }
+
+    @Test
+    public void testPrefixSearch05() {
+        char[] test = "abcdefffff".toCharArray();
+        int[] result = LempelZiv.findPrefix(test, 5, test.length, 0, 5);
+        assertNull(result);
+    }
+
+    @Test
+    public void testPrefixSearch06() {
+        char[] test = "abcddddddd".toCharArray();
+        int[] result = LempelZiv.findPrefix(test, 3, test.length, 0, 3);
+        assertNull(result);
+    }
+
+    @Test
+    public void testPrefixSearch07() {
+        char[] test = "abcdefghhh".toCharArray();
+        int[] result = LempelZiv.findPrefix(test, 7, test.length, 0, 7);
+        assertNull(result);
+    }
+
+    @Test
+    public void testPrefixSearch08() {
+        char[] test = "abcabcefgh".toCharArray();
+        int[] result = LempelZiv.findPrefix(test, 3, test.length, 0, 3);
+        assertEquals(0, result[0], "Best match index was wrong.");
+        assertEquals(3, result[1], "Best match length was wrong.");
+    }
+
+    @Test
+    public void testPrefixSearch09() {
+        char[] test = "abcdefgabc".toCharArray();
+        int[] result = LempelZiv.findPrefix(test, 7, test.length, 0, 7);
+        assertEquals(0, result[0], "Best match index was wrong.");
+        assertEquals(3, result[1], "Best match length was wrong.");
     }
 }
