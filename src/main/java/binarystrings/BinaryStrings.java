f31dd417fe615ff8f183c33e2c94adf2e60315df
NightFish <nightfish.net@gmail.com>
2020-06-22 00:57:36 +1200

    Fix log calculation bug

--- src/main/java/binarystrings/BinaryStrings.java
+++ src/main/java/binarystrings/BinaryStrings.java
@@ -6,7 +6,7 @@ public class BinaryStrings {
     public static String encode(String a, String b) {
         int totalLength = a.length() + b.length();
         String binary = Integer.toBinaryString(a.length());
-        int binaryBits = (int)(Math.log(totalLength + 1) / Math.log(2) + 1e-10);
+        int binaryBits = (int)(Math.log(totalLength + 1) / Math.log(2));
         while (binary.length() < binaryBits) {
             binary = "0" + binary;
         }
@@ -16,9 +16,9 @@ public class BinaryStrings {
     public static Pair<String, String> decode(String c) {
         int n = c.length();
         while (n > 0) {
-            double l = Math.log(n + 1) / Math.log(2) + 1e-10;
+            double l = Math.log(n + 1) / Math.log(2);
 
-            if(l + n < c.length()) {
+            if(l + n <= c.length()) {
                 int aLength = Integer.valueOf(c.substring(0, (int)l), 2);
                 System.out.println(aLength);
                 String a = c.substring((int)l, aLength + (int)l);
--- src/test/java/binarystrings/BinaryTest.java
+++ src/test/java/binarystrings/BinaryTest.java
@@ -5,6 +5,7 @@ import main.java.binarystrings.BinaryStrings;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class BinaryTest {
     @Test
@@ -20,6 +21,24 @@ public class BinaryTest {
     }
 
     @Test
+    public void TestEncode03() {
+        String encoded = BinaryStrings.encode("11101010010111011010110101101011010110100", "101010111111101010100100100010011110110110101010");
+        assertEquals("10100111101010010111011010110101101011010110100101010111111101010100100100010011110110110101010", encoded);
+    }
+
+    @Test
+    public void TestEncode04() {
+        String encoded = BinaryStrings.encode("", "1");
+        assertEquals("01", encoded);
+    }
+
+    @Test
+    public void TestEncode05() {
+        String encoded = BinaryStrings.encode("1", "");
+        assertEquals("11", encoded);
+    }
+
+    @Test
     public void TestDecode01() {
         Pair<String, String> decoded = BinaryStrings.decode("110");
         assertEquals("1", decoded.getKey());
@@ -32,4 +51,19 @@ public class BinaryTest {
         assertEquals("110", decoded.getKey());
         assertEquals("100", decoded.getValue());
     }
+
+    @Test
+    public void TestDecode03() {
+        Pair<String, String> decoded = BinaryStrings.decode("10100111101010010111011010110101101011010110100101010111111101010100100100010011110110110101010");
+        assertEquals("11101010010111011010110101101011010110100", decoded.getKey());
+        assertEquals("101010111111101010100100100010011110110110101010", decoded.getValue());
+    }
+
+    @Test
+    public void TestDecode04() {
+        Pair<String, String> decoded = BinaryStrings.decode("11");
+        assertEquals("1", decoded.getKey());
+        assertEquals("", decoded.getValue());
+    }
+
 }
