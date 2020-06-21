dca8d9d083eb36e2b6294b406029f7c9c6c08ea0
NightFish <nightfish.net@gmail.com>
2020-06-22 01:07:45 +1200

    Improves notes and adds testing for binary

--- src/test/java/binarystrings/BinaryTest.java
+++ src/test/java/binarystrings/BinaryTest.java
@@ -5,7 +5,6 @@ import main.java.binarystrings.BinaryStrings;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class BinaryTest {
     @Test
