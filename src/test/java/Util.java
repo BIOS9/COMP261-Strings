eedd6acc2fd8f057dfc92d421cc21c3919b04494
NightFish <nightfish.net@gmail.com>
2020-06-21 16:48:52 +1200

    Remove inaccurate code attribution

--- src/test/java/Util.java
+++ src/test/java/Util.java
@@ -8,8 +8,7 @@ import java.nio.file.Paths;
 
 public class Util {
     private static final Charset CHARSET = StandardCharsets.UTF_8;
-
-    // Took this code from https://www.journaldev.com/875/java-read-file-to-string
+    
     public static String readString(String path) throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, CHARSET);
